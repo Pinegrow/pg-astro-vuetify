c4f0c935e5b620b8a80bf1985c8408b27aa4281d
Akayy <techakayy@gmail.com>
2024-07-15 19:08:44 +1000

    Updated vue devtools next

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import AutoImportComponents from 'unplugin-vue-components/vite'
 import AutoImportAPIs from 'unplugin-auto-import/astro'
 import Unocss from 'unocss/vite'
 import presetIcons from '@unocss/preset-icons'
-// import VueDevTools from 'vite-plugin-vue-devtools'
+import VueDevTools from 'vite-plugin-vue-devtools'
 // import myAstroModule from './src/modules/my-module'
 
 // import { visualizer } from 'rollup-plugin-visualizer'
@@ -164,7 +164,9 @@ export default defineConfig({
           )
         },
       },
-      // VueDevTools()
+      VueDevTools({
+        appendTo: 'app.ts',
+      }),
     ],
 
     // build: {
