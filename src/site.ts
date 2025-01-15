7a5be45e2714012a4e71a88a5090fc641d138592
Akayy <techakayy@gmail.com>
2025-01-16 08:48:12 +1100

    Minor update

--- src/site.ts
+++ src/site.ts
@@ -26,11 +26,11 @@ export default {
       },
     ],
     secondary: [
-      {
-        title: 'Subscribe',
-        icon: 'i-mdi-home',
-        to: '/subscribe',
-      },
+      // {
+      //   title: 'Subscribe',
+      //   icon: 'i-mdi-home',
+      //   to: '/subscribe',
+      // },
     ],
   },
 }
